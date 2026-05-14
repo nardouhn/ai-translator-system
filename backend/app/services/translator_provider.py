73a1124135f848970e2ef8a83ab5de10ec12d29e
Tuyet Nhu Nguyen <tuyetnhu@email.com>
2026-05-15 05:12:14 +0700

    fix translation api link to remove /translate path

--- backend/app/services/translator_provider.py
+++ backend/app/services/translator_provider.py
@@ -6,7 +6,7 @@ from fastapi import HTTPException
 # Setup logger
 logger = logging.getLogger(__name__)
 
-CUSTOM_MODEL_URL = "https://liability-uncharted-identity.ngrok-free.dev/translate"
+CUSTOM_MODEL_URL = "https://liability-uncharted-identity.ngrok-free.dev/"
 
 # Limit concurrent requests to the local AI model to avoid overloading
 # We use 2 to ensure stability with single GPU on Kaggle
