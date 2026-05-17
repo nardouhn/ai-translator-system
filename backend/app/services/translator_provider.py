c68d4770c3a4eca56aa44234a0a4ca35cdd644c7
Tuyet Nhu Nguyen <tuyetnhu@email.com>
2026-05-17 09:36:56 +0700

    update model

--- backend/app/services/translator_provider.py
+++ backend/app/services/translator_provider.py
@@ -6,7 +6,7 @@ from fastapi import HTTPException
 # Setup logger
 logger = logging.getLogger(__name__)
 
-CUSTOM_MODEL_URL = "https://liability-uncharted-identity.ngrok-free.dev/translate"
+CUSTOM_MODEL_URL = "http://34.126.182.8/translate"
 
 # Limit concurrent requests to the local AI model to avoid overloading
 # We use 2 to ensure stability with single GPU on Kaggle
